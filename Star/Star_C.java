14595aa144f65171fb69639655694d668ddef33a
Sleepy <71368659+edselph@users.noreply.github.com>
2021-03-08 09:53:21 +0700

    Add files via upload

---
+++ Star/Star_C.java
@@ -0,0 +1,15 @@
+public class Star_C {
+  public static void main(String args[]) {  
+    int i, j, row = 9;       
+    for (i=0; i<row; i+=2){       
+    for (j=row-i; j>1; j--){ 
+    System.out.print(" ");   
+    } 
+    for (j=0; j<=i; j++ ){        
+    System.out.print("* ");   
+    }   
+    System.out.println();   
+    }   
+    }   
+}
+
